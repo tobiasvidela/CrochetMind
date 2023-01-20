78e53b20b3cf65deeb328b6e362c534d49cfd679
Tobias Videla G <tobiasvidelaug14@gmail.com>
2023-01-19 23:31:38 -0300

    Added JS interaction

---
+++ password.js
@@ -0,0 +1,17 @@
+const password = document.getElementById('password'),
+    eye = document.getElementById('eye'),
+    icon = document.getElementById('icon-input');
+
+let state = false
+
+eye.addEventListener('click', () => {
+    if (state) {
+        password.setAttribute('type','password')
+        eye.style.color = '#312BB3'
+        state = false
+    } else {
+        password.setAttribute('type','text')
+        eye.style.color = '#6587FD'
+        state = true
+    }
+})

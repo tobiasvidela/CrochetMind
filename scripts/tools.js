fbf09b212af41d8798b8f999aa8269ebc183a9aa
Tobias Videla G <tobiasvidelaug14@gmail.com>
2023-01-27 15:38:06 -0300

    adding CircleRaises function

--- scripts/tools.js
+++ scripts/tools.js
@@ -81,17 +81,48 @@ function getSpiralRaises(initial, final) {
         return spiralRaises;
     };
     
-    let acum = Number(initial);
+    let acum = initial;
     
-    for (let i = 3, sc = 1; acum < final; i++) { //generate raises until final points are reached
+    //generate raises until final points are reached
+    for (let i = 3, sc = 1; acum < final; i++) { // sc stands for single crochet quantity
         acum = initial * i;
         spiralRaises += `${i} ~ (${sc} sc, inc)*${initial} [${initial * i}] ~<br>`;
         sc++;
     };
 
     return spiralRaises;
-}
+};
 
+function getCircleRaises(initial, final) { //get spiral raises with compensations
+    let circleRaises = `1 ~ ${initial} sc [${initial}] ~ (start)<br>`;
+    
+    circleRaises += `2 ~ (1 inc)*${initial} [${initial * 2}] ~<br>`;
+
+    if (final == initial * 2) {
+        return circleRaises;
+    };
+    
+    circleRaises += `3 ~ (inc, ${initial} sc)*${initial} [${initial * 3}] ~<br>`;
+    
+    if (final == initial * 3) {
+        return circleRaises;
+    };
+
+    let acum = initial, c = 1; //c stands for uneven rows compensation singlecrochets
+
+    for (let i = 4, sc = 2; acum < final; i++) {
+        acum = initial * i;
+        if (i % 2 == 0) {
+            circleRaises += `${i} ~ (${sc} sc, inc)*${initial} [${initial * i}] ~<br>`;
+        } else {
+            circleRaises += `${i} ~ ${c} sc, (${sc} sc, inc)*${initial-1}, inc, ${c+1} sc [${initial * i}] ~<br>`;
+            c++;
+        };
+        sc++;
+    };
+
+    return circleRaises;
+};
 
 function validatePoints(initial, final, pat) { //validate points entered
     if (initial == 0 || final == 0) {
